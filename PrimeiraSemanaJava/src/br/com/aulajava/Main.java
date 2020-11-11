9df500a577cc50ec66e8d3bd48a48ffebbbf8e29
Your Name <you@example.com>
2020-11-11 12:46:34 -0300

    Feito

---
+++ PrimeiraSemanaJava/src/br/com/aulajava/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.aulajava;
+
+/**
+ * @author samue
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
